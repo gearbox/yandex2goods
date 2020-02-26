44fbcedb4fc3fdaca0211c2d27486b7aebab9d1d
gearbox <mtayrov@gmail.com>
2020-02-26 22:11:31 +0300

    Fix JS ajaxs issues

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask import send_from_directory
 
 import xls_to_xml
@@ -35,18 +35,34 @@ def serve_files(filename):
 def convert_file():
     # check if the post request has the file part
     if 'file' not in request.files:
-        flash('No file part', 'warning')
-        return redirect(request.path)
-        # return redirect(url_for('index'))
+        # flash('No file part', 'warning')
+        resp = jsonify({'message': 'No file part.', 'type': 'warning'})
+        # resp.status_code = 400
+        return resp
+        # return redirect(request.path)
     file = request.files['file']
     # if user does not select file, browser also submit an empty part without filename
     if file.filename == '':
-        flash('Не выбран ни один файл', 'warning')
-        return redirect(url_for('index'))
+        # flash('Не выбран ни один файл', 'warning')
+        resp = jsonify({'message': 'Не выбран ни один файл.', 'type': 'warning'})
+        # resp.status_code = 400
+        return resp
+        # return redirect(url_for('index'))
     if not xls_to_xml.allowed_filetype(file.filename):
-        flash('Данный тип файлов не поддерживается', 'danger')
-        return redirect(url_for('index'))
+        # flash('Данный тип файлов не поддерживается', 'danger')
+        resp = jsonify({'message': 'Данный тип файлов не поддерживается.', 'type': 'danger'})
+        # resp.status_code = 500
+        return resp
+        # return redirect(url_for('index'))
     filename = xls_to_xml.convert(file)
+    if filename:
+        resp = jsonify({
+            'message': 'Файл успешно сконвертирован.',
+            'type': 'success',
+            'link': url_for('serve_files', filename=filename.name),
+            'filename': filename.name
+        })
+        return resp
     # return redirect(url_for('serve_files', filename=filename.name))
     return render_template('converted.html', filename=filename)
 
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -3,7 +3,7 @@ $(function() {
         let form_data = new FormData($('#upload-file')[0]);
         let ins = document.getElementById('file').files.length;
         if(ins === 0) {
-					$('#fl-message').html(
+					$('#status-message').html(
 					    '<div class="alert alert-warning alert-dismissible" role="alert">' +
                         '        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
                         '        Не выбран ни один файл.</div>');
@@ -12,41 +12,48 @@ $(function() {
         $.ajax({
             type: 'POST',
             url: '/convert-xls', // point to server-side URL
-            dataType: 'html', // what to expect back from server
+            dataType: 'json', // what to expect back from server
             data: form_data,
             contentType: false,
             cache: false,
             processData: false,
             success: function(response) {
-                $('#fl-message').html(
-                    '<div class="alert alert-success alert-dismissible" role="alert">' +
-                    '        <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' +
-                    '        Файл успешно сконвертирован.</div>');
-            //         let json = jQuery.parseJSON(response);
-            //         $('#fl-message').html(json.message);
-                    console.log(response);
+                $('#status-message').html(
+                    '<div class="alert alert-' + response.type + ' alert-dismissible" role="alert">' +
+                    '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+                    '<span aria-hidden="true">&times;</span></button>' +
+                    response.message + '</div>'
+                );
+                $.each(response, function (key, data) {
+                    if (data === 'success') {
+                        $('#conv-result').html('<h3>XML для Goods.ru:</h3>');
+                        $('#download-links').append(
+                            '<a href="' + response.link + '">Скачать ' + response.filename + '</a><br/>'
+                        );
+                        // $('#download-links a').each(
+                            // function () {
+                                // let links = this.attr('href');
+                                // for (let i = 0, len = links.length; i < len; i++) {
+                                //     if (location.href.search(this.attr('href')) !== $('a').attr('href')) {
+                                //         $('#download-links').append(
+                                //             '<a href="' + response.link + '">Скачать ' + response.filename + '</a><br/>'
+                                //         );
+                                //     }
+                                // }
+                            // })
+                    }
+                });
+                console.log(response);
                 },
-                error: function(error) {
-                    console.log(error);
-                }
-            // success: function(data) {
-            //     console.log('Success!');
-            //     if (data.redirect) {
-                    // if( data.load_html) {
-                    // data.redirect contains the string URL to redirect to
-                    // window.location.href = data.redirect;
-
-                    // window.location.replace(data.redirect);
-
-                    // $("#conv-result").text(data.result);
-                // } else {
-
-                    // data.form contains the HTML for the replacement form
-                    // $("#my_form").replaceWith(data.form);
-
-                // }
-
-            // },
+            error: function(error) {
+                $('#status-message').html(
+                    '<div class="alert alert-' + error.responseText.type + ' alert-dismissible" role="alert">' +
+                    '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+                    '<span aria-hidden="true">&times;</span></button>' +
+                    error.responseText.message + '</div>'
+                );
+                console.log(error);
+            }
         });
     });
 });
